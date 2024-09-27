533cd60904230bf70016a32fdf5f5de5d751d805
simokhin <nick.simokhin@yandex.ru>
2024-09-27 03:14:15 +0300

    Add more sounds

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 window.addEventListener("keydown", (e) => {
-    console.log(e)
     const audio = document.querySelector(`audio[id="${e.key}"]`);
+    const buttons = document.querySelector(`button[id="${e.key}"]`)
     audio.currentTime = 0;
     audio.play();
-})
+    buttons.classList.add("playing");
+})  
 
+window.addEventListener("keyup", (e) => {
+    const buttons = document.querySelector(`button[id="${e.key}"]`)
+    buttons.classList.remove("playing");
+})
